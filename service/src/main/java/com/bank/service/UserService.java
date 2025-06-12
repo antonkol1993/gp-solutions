7263d8c8b1b23e49c489a78104823209c4b68a86
Anton office <antonkol1993@gmail.com>
2025-06-12 17:21:58 +0300

    service 1. добавлен UserService dao 1. добавлен UserService UserRepository
    Скорректированы зависимости между модулями

---
+++ service/src/main/java/com/bank/service/UserService.java
@@ -0,0 +1,25 @@
+package com.bank.service;
+
+import com.bank.dao.repository.UserRepository;
+import com.bank.model.entity.UserEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<UserEntity> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public UserEntity save(UserEntity user) {
+        return userRepository.save(user);
+    }
+}
