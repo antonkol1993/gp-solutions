e31a4712d635c3cb568b0e113d984c33e2b9236f
Anton office <antonkol1993@gmail.com>
2025-06-12 16:56:33 +0300

    1. Добавлены зависимости между модулями 2. указаны актуальные библиотеки для : model, dao, api-app

---
+++ model/src/main/java/com/bank/model/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.bank.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+}
